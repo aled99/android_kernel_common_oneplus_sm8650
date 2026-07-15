b1915c51c2de59bf6b374180d09e5a416cee88ba
John Galt <johngaltfirstrun@gmail.com>
2026-07-15 10:12:51 -0400

    mm/vsmscan: move task_is_critical outside of LRU_GEN scope

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -3456,6 +3456,19 @@ static bool can_age_anon_pages(struct pglist_data *pgdat,
 	return can_demote(pgdat->node_id, sc);
 }
 
+#define CRITICAL_OOM_SCORE_ADJ	(-900)
+
+static __always_inline bool task_is_critical(void)
+{
+	if (current->flags & PF_KTHREAD)
+		return false;
+
+	if (unlikely(!current->signal))
+		return false;
+
+	return READ_ONCE(current->signal->oom_score_adj) <= CRITICAL_OOM_SCORE_ADJ;
+}
+
 #ifdef CONFIG_LRU_GEN
 
 #if defined(CONFIG_LRU_GEN_ENABLED) && !defined(CONFIG_CONT_PTE_HUGEPAGE)
@@ -5304,19 +5317,6 @@ static int scan_folios(struct lruvec *lruvec, struct scan_control *sc,
 	 * remaining to prevent livelock if it's not making progress.
 	 */
 	return isolated || !remaining ? scanned : 0;
-}
-
-#define CRITICAL_OOM_SCORE_ADJ	(-900)
-
-static __always_inline bool task_is_critical(void)
-{
-	if (current->flags & PF_KTHREAD)
-		return false;
-
-	if (unlikely(!current->signal))
-		return false;
-
-	return READ_ONCE(current->signal->oom_score_adj) <= CRITICAL_OOM_SCORE_ADJ;
 }
 
 static int get_tier_idx(struct lruvec *lruvec, int type)
