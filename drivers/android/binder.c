1977993bb2ba7dcbe4e25031c7a168bf1767cdd0
John Galt <johngaltfirstrun@gmail.com>
2026-07-21 17:31:40 -0400

    binder: force sync wakeup for critical task transactions
    Reduces wakeup latency for critical tasks, as without sync we run
    wake_up_interruptible, and with sync we run wake_up_interruptible_sync.
    
    Based on possibilities of the vendor hook
    android_vh_binder_wakeup_ilocked.

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -680,6 +680,10 @@ static void binder_wakeup_thread_ilocked(struct binder_proc *proc,
 {
 	assert_spin_locked(&proc->inner_lock);
 
+	/* Force sync wakeup when the sender is a critical task */
+	if (task_is_critical())
+		sync = true;
+
 	if (thread) {
 		trace_android_vh_binder_wakeup_ilocked(thread->task, sync, proc);
 		if (sync)
