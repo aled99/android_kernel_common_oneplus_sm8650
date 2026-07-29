73996e5e54e4f19bd706e3c3c49844c1541bc5c4
ale99 <51166204+aled99@users.noreply.github.com>
2026-07-29 04:59:51 +0000

    Revert "sched/fair: Fix pelt clock sync when entering idle"
    This reverts commit ec20910a3f3da927b3d57a27dd0c1ab2a70ffb2e.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8058,6 +8058,12 @@ idle:
 	if (new_tasks > 0)
 		goto again;
 
+	/*
+	 * rq is about to be idle, check if we need to update the
+	 * lost_idle_time of clock_pelt
+	 */
+	update_idle_rq_clock_pelt(rq);
+
 	return NULL;
 }
 
