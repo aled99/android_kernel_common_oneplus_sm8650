46530498fd11c78a1a6af71855a0fd702a3e13e7
ale99 <51166204+aled99@users.noreply.github.com>
2026-08-02 16:00:13 +0000

    Reapply "binder: bypass priority inheritance cap for critical task senders"
    This reverts commit c9e384139dbd2b0fc6bc79d90dd392c84b2282ed.

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -77,6 +77,20 @@
 #include "binder_internal.h"
 #include "binder_trace.h"
 #include <trace/hooks/binder.h>
+#include "../../kernel/sched/sched.h"
+
+#define CRITICAL_OOM_SCORE_ADJ	(-900)
+
+static __always_inline bool task_is_critical(void)
+{
+	if (current->flags & PF_KTHREAD)
+		return false;
+
+	if (unlikely(!current->signal))
+		return false;
+
+	return READ_ONCE(current->signal->oom_score_adj) <= CRITICAL_OOM_SCORE_ADJ;
+}
 
 static HLIST_HEAD(binder_deferred_list);
 static DEFINE_MUTEX(binder_deferred_lock);
@@ -851,7 +865,8 @@ static void binder_transaction_priority(struct binder_thread *thread,
 	if (skip)
 		return;
 
-	if (!node->inherit_rt && is_rt_policy(desired.sched_policy)) {
+	if (!task_is_critical() &&
+	    !node->inherit_rt && is_rt_policy(desired.sched_policy)) {
 		desired.prio = NICE_TO_PRIO(0);
 		desired.sched_policy = SCHED_NORMAL;
 	}
@@ -888,7 +903,10 @@ static void binder_transaction_priority(struct binder_thread *thread,
 	}
 	spin_unlock(&thread->prio_lock);
 
-	binder_set_priority(thread, &desired);
+	if (task_is_critical())
+		binder_do_set_priority(thread, &desired, false);
+	else
+		binder_set_priority(thread, &desired);
 	trace_android_vh_binder_set_priority(t, task);
 }
 
